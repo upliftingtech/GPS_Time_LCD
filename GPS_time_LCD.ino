a16fad5f6e56f95f0d84842d0fc1c29cc2be897e
upliftingtech <77591525+upliftingtech@users.noreply.github.com>
2021-01-28 08:00:24 -0800

    Create GPS_time_LCD.ino

---
+++ GPS_time_LCD.ino
@@ -0,0 +1,98 @@
+// Code by Brian Ernst 2021
+// GPS parsing code from GPS_SoftwareSerial_Parsing by Adafruit - adafruit.com
+
+// Arduino Uno
+// GPS: Adafruit Ultimate GPS module using MTK33x9 chipset - http://www.adafruit.com/products/746
+
+// Displaying on LCD117 by moderndevice.com
+// Commands: https://moderndevice.com/wp-content/uploads/resources/lcd117-kitlcd/LCD117_Board_Command_Summary.pdf
+
+
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+
+// Connect the GPS Power pin to 5V
+// Connect the GPS Ground pin to ground
+// Connect the GPS TX (transmit) pin to Digital 8
+// Connect the GPS RX (receive) pin to Digital 7
+
+SoftwareSerial mySerial(8, 7);
+Adafruit_GPS GPS(&mySerial);
+
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences
+#define GPSECHO  false
+
+void setup()
+{
+  Serial.begin(9600); // my lcd screen is this speed, but this might be too slow if gps echoing is turned on.
+                      // sample code used 115200 and noted: connect at 115200 so we can read the GPS fast enough and echo without dropping chars
+  
+  delay(5000); // give a moment for gps chip to start. probably overkill
+  
+  Serial.print("?f"); // clear screen
+  Serial.println("Test serial out");
+
+  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  GPS.begin(9600);
+
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+  // the parser doesn't care about other sentences at this time
+
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  // For the parsing code to work nicely and have time to sort thru the data, and
+  // print it out we don't suggest using anything higher than 1 Hz
+
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  delay(1000);
+  // Ask for firmware version
+  mySerial.println(PMTK_Q_RELEASE);
+}
+
+uint32_t timer = millis();
+void loop()                     // run over and over again
+{
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+  if ((c) && (GPSECHO))
+    Serial.write(c);
+
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences!
+    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
+
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+  }
+
+  // approximately every 2 seconds or so, print out the current stats
+  if (millis() - timer > 2000) {
+    timer = millis(); // reset the timer
+
+    Serial.print("?f"); // clear screen
+    Serial.print("T: ");
+    if (GPS.hour < 10) { Serial.print('0'); }
+    Serial.print(GPS.hour, DEC); Serial.print(':');
+    if (GPS.minute < 10) { Serial.print('0'); }
+    Serial.print(GPS.minute, DEC); Serial.print(':');
+    if (GPS.seconds < 10) { Serial.print('0'); }
+    Serial.print("?n"); //Carriage Return / Line Feed. Put cursor at start of next line, line cleared
+    Serial.print("F: "); Serial.print((int)GPS.fix);
+    Serial.print(" Q: "); Serial.println((int)GPS.fixquality);
+    if (GPS.fix)
+    {
+      Serial.print(" S: ");
+      Serial.print((int)GPS.satellites);
+    }
+  }
+}
